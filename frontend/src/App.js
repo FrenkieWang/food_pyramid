bda217866a1aa56cfa69e7a74fee3ccc590069d6
FrenkieWang <FrenkieWang21@gmail.com>
2024-02-14 10:32:31 +0000

    Add a rectangle container with border

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,16 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      <div className="color-bar red">red</div>
-      <div className="spacer">white</div>
-      <div className="color-bar orange">orange</div>
-      <div className="spacer">white</div>
-      <div className="color-bar yellow">yellow</div>
-      <div className="color-bar blue">blue</div>
-      <div className="color-bar brown">brown</div>
-      <div className="color-bar green">green</div>
+      <div className="container">
+        <div className="color-bar red">red</div>
+        <div className="spacer">white</div>
+        <div className="color-bar orange">orange</div>
+        <div className="spacer">white</div>
+        <div className="color-bar yellow">yellow</div>
+        <div className="color-bar blue">blue</div>
+        <div className="color-bar brown">brown</div>
+        <div className="color-bar green">green</div>
+      </div>
     </div>
   );
 };

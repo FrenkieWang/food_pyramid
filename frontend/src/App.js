0e278cc8e08da3a1488285464d5f49c14f3770c6
FrenkieWang <FrenkieWang21@gmail.com>
2024-02-14 10:18:23 +0000

    Add CSS file and center align

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,17 @@
-import React, {useState} from 'react';
+import React from 'react';
+import './App.css'; 
 
 const App = () => {
   return (
     <div className="App">
-      <div style={{ backgroundColor: 'red', height: '30px', width: '100%' }}>red</div>
-      <div style={{ backgroundColor: 'white', height: '10px', width: '100%' }}>white</div>
-      <div style={{ backgroundColor: 'orange', height: '30px', width: '100%' }}>orange</div>
-      <div style={{ backgroundColor: 'white', height: '10px', width: '100%' }}>white</div>
-      <div style={{ backgroundColor: 'yellow', height: '30px', width: '100%' }}>yellow</div>
-      <div style={{ backgroundColor: 'blue', height: '30px', width: '100%' }}>blue</div>
-      <div style={{ backgroundColor: 'brown', height: '30px', width: '100%' }}>brown</div>
-      <div style={{ backgroundColor: 'green', height: '30px', width: '100%' }}>green</div>
+      <div className="color-bar red">red</div>
+      <div className="spacer">white</div>
+      <div className="color-bar orange">orange</div>
+      <div className="spacer">white</div>
+      <div className="color-bar yellow">yellow</div>
+      <div className="color-bar blue">blue</div>
+      <div className="color-bar brown">brown</div>
+      <div className="color-bar green">green</div>
     </div>
   );
 };

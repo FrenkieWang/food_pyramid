2ee40885ddb12f132c530704b1ee4392e22a1205
FrenkieWang <FrenkieWang21@gmail.com>
2024-02-14 10:06:50 +0000

    Create 6 Color-Bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,14 @@ import React, {useState} from 'react';
 const App = () => {
   return (
     <div className="App">
-      hello!
+      <div style={{ backgroundColor: 'red', height: '30px', width: '100%' }}>red</div>
+      <div style={{ backgroundColor: 'white', height: '10px', width: '100%' }}>white</div>
+      <div style={{ backgroundColor: 'orange', height: '30px', width: '100%' }}>orange</div>
+      <div style={{ backgroundColor: 'white', height: '10px', width: '100%' }}>white</div>
+      <div style={{ backgroundColor: 'yellow', height: '30px', width: '100%' }}>yellow</div>
+      <div style={{ backgroundColor: 'blue', height: '30px', width: '100%' }}>blue</div>
+      <div style={{ backgroundColor: 'brown', height: '30px', width: '100%' }}>brown</div>
+      <div style={{ backgroundColor: 'green', height: '30px', width: '100%' }}>green</div>
     </div>
   );
 };

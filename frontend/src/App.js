09dbb5c59731475a01513d0ef580c00af5833874
FrenkieWang <FrenkieWang21@gmail.com>
2024-02-16 06:39:23 +0000

    Switch Pyramid Recommendations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
-  // Initialize the State of Layer
-  const initialLayers = {
+
+  const initialAdultLayers = {
     red: { num: 1, height: 30, colorState: 'normal' },
     orange: { num: 1, height: 30, colorState: 'normal' },
     yellow: { num: 2, height: 40, colorState: 'normal' },
@@ -11,17 +11,9 @@ const App = () => {
     darkgoldenrod : { num: 4, height: 60, colorState: 'normal' },
     limegreen: { num: 6, height: 80, colorState: 'normal' }
   };
-  const [layers, setLayers] = useState(initialLayers);
+  const [layers, setLayers] = useState(initialAdultLayers);
 
-  const today = new Date().toISOString().split('T')[0];
-
-  const formatDate = (dateString) => {
-    const [year, month, day] = dateString.split('-');
-    return `[${day}-${month}-${year}]`;
-  };
-
-  // Define the valid range for each layer
-  const validRanges = {
+  const initialAdultValidRanges = {
     red: [0, 0],
     orange: [0, 1],
     yellow: [2, 2],
@@ -29,15 +21,50 @@ const App = () => {
     darkgoldenrod: [3, 5],
     limegreen: [5, 7]
   };
+  const [validRanges, setValidRanges] = useState(initialAdultValidRanges);
+
+  const today = new Date().toISOString().split('T')[0];
+  const formatDate = (dateString) => {
+    const [year, month, day] = dateString.split('-');
+    return `[${day}-${month}-${year}]`;
+  };
+
+  const [age, setAge] = useState('Adult');  
+  const changeAge = () => {
+    const newAge = (age === 'Adult' ? 'Child' : 'Adult'); 
+    setAge(newAge);
+
+    // Change Valid Ranges   
+    setValidRanges(prevValidRanges => {
+      const newValidRanges = {
+        ...prevValidRanges,
+        dodgerblue: newAge === 'Child' ? [5, 5] : [3, 3],
+        darkgoldenrod: newAge === 'Child' ? [3, 7] : [3, 5]
+      };      
+
+      return newValidRanges;
+    });
+
+    // Change initial Layers Status in valid ranges
+    setLayers(() => {
+      const newLayers = initialAdultLayers;
+
+      if (newAge === 'Child') {
+        newLayers.dodgerblue = { num: 5, height: 70, colorState: 'normal' };
+      } 
+
+      return newLayers;
+    });    
+  };
 
   // Handle increase/decrease button click events
-  const handleChange = (color, diff, index) => {
+  const handleButtonEvents = (color, diff, index) => {
     setLayers((prevLayers) => {
       const newLayer = { ...prevLayers };
       const newNum = Math.max(0, Math.min(9, newLayer[color].num + diff)); // num range [0 - 9]
 
-      const initialNum = initialLayers[color].num;
-      const initialHeight = initialLayers[color].height;
+      const prevNum = layers[color].num;
+      const prevHeight = layers[color].height;
 
       const [min, max] = validRanges[color];
       let newColorState = 'normal';
@@ -51,7 +78,12 @@ const App = () => {
       // Only num 0 in Layer 1 is normal
       if(color === 'red' && newNum === 0) newColorState = 'lightpink'
 
-      newLayer[color] = { num: newNum, height: initialHeight + (newNum - initialNum) * 10, colorState: newColorState }; // Change height of that Layer
+      // Change number and height of this layer
+      newLayer[color] = { 
+        num: newNum, 
+        height: prevHeight + (newNum - prevNum) * 10, colorState: newColorState 
+      };
+
       return newLayer;
     });
   };
@@ -62,8 +94,11 @@ const App = () => {
 
   return (
     <div className="App">
-      <div className="title text">
-        Figure 1
+      <div className = "title">
+        Switch Pyramid &nbsp;
+        <button className = "ageSelect" onClick={changeAge}>
+          {age}
+        </button>
       </div>
       <div className="Pyramid">
         <div className="container" style={{ width:`${containerWidth}px`, height: `${containerHeight}px` }}>
@@ -73,15 +108,16 @@ const App = () => {
                 height: `${layers[color].height}px`, 
                 backgroundColor: layers[color].colorState === 'normal' ? color : layers[color].colorState
               }}>
-                <button className="btn ctrl" onClick={() => handleChange(color, 1, index + 1)}>+</button>
+                <button className="btn ctrl" onClick={() => handleButtonEvents(color, 1, index + 1)}>+</button>
                 <button className="btn num">{layers[color].num}</button>
-                <button className="btn ctrl" onClick={() => handleChange(color, -1, index + 1)}>-</button>
+                <button className="btn ctrl" onClick={() => handleButtonEvents(color, -1, index + 1)}>-</button>
               </div>
               {/* Add Spacer only between 1-2 and 2-3 Layer*/}
               {index === 0 || index === 1 ? <div className="spacer"></div> : null}
             </React.Fragment>
           ))}
         </div>
+
         {/* Use two white triangle to cover the container */}
         <div className="cover-container" style={{ 
           borderLeft: `${containerWidth / 2 + 1}px solid white`,  /* +1 Make it cover the border */
@@ -89,6 +125,7 @@ const App = () => {
           borderBottom: `${containerHeight + 1}px solid transparent` 
         }}></div>
       </div>
+
       <div className = "user text">My Food Pyramid</div>
       <div className = "date text">{formatDate(today)}</div>
     </div>
